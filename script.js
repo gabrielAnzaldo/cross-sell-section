0daeff76f165b6a8c2cf927024afb152198afc8a
Gabriel <anzaldo.gabriel.dev@gmail.com>
2024-06-24 16:53:08 -0400

    Click event listerner added on red more CTA.

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("script loaded..");
+const readMore = document.getElementById("show-more");
+readMore.addEventListener("click", function () {
+  console.log("click on read more...");
+});
